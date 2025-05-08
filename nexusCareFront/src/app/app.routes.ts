275239d36dcb35290207185da4e885962c3b1eab
lucasbennertz <lucasgcbennertz@gmail.com>
2025-05-08 18:01:46 -0300

    update adição de rota para about

--- nexusCareFront/src/app/app.routes.ts
+++ nexusCareFront/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: SobreComponent
   }
 ];
