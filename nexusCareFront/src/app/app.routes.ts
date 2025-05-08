5a3d66b0a06223a610224a1a0425a0bd052c5a47
dennerborba <dennerdeborba@gmail.com>
2025-05-08 18:55:25 -0300

    resolvendo conflitos

--- nexusCareFront/src/app/app.routes.ts
+++ nexusCareFront/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RelatorioComponent } from './pages/relatorio/relatorio.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'relatorio',
+    component: RelatorioComponent
   },
   {
     path: 'about',
