1dda2375bb760615b3cf3ec0a6666e2ba042e2ea
lucasbennertz <lucasgcbennertz@gmail.com>
2025-05-09 17:32:36 -0300

    get dos medicos está funcionando

---
+++ nexusCareFront/src/app/models/paciente.model.ts
@@ -0,0 +1,9 @@
+export interface Paciente {
+  id: number;
+  nome_completo: string;
+  endereco_completo: string;
+  codigo_pronto: string;
+  codigo_cns: string;
+  sexo: string;
+  data_nascimento: string;
+}
