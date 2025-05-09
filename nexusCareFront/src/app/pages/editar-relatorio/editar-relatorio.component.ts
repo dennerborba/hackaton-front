fbaacc23739a7e49cc6a2d9a89d32ac82258c21f
dennerborba <dennerdeborba@gmail.com>
2025-05-08 21:27:54 -0300

    add angular material

---
+++ nexusCareFront/src/app/pages/editar-relatorio/editar-relatorio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-relatorio',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-relatorio.component.html',
+  styleUrl: './editar-relatorio.component.css'
+})
+export class EditarRelatorioComponent {
+
+}
