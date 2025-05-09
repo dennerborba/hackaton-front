1f21073342425e503d03f14b5bea3e9386839964
lucasbennertz <lucasgcbennertz@gmail.com>
2025-05-09 19:10:40 -0300

    inicio da tela de relatorio editavel

---
+++ nexusCareFront/src/app/pages/relatorio-editavel/relatorio-editavel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorio-editavel',
+  standalone: true,
+  imports: [],
+  templateUrl: './relatorio-editavel.component.html',
+  styleUrl: './relatorio-editavel.component.css'
+})
+export class RelatorioEditavelComponent {
+
+}
